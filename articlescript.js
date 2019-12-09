95c6b35b1b5ab1e804283d05ae77003f028d242b
lourdesgalindez <54605023+lourdesgalindez@users.noreply.github.com>
2019-12-09 16:19:51 -0500

    Add files via upload

---
+++ articlescript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.materialboxed');
+   var instances = M.Materialbox.init(elems);
+ });
+
+ document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.slider');
+   var instances = M.Slider.init(elems);
+ });
